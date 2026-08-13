8f3a0d36d8ccf734e1e8544837bbd3367e2f78c3
MD. Taiyab Ur Rahman <taiyaburrahman2742@gmail.com>
2026-08-13 22:57:22 +0600

    Configure database for local and live environments

--- backend/config.php
+++ backend/config.php
@@ -1,9 +1,20 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "shopsphere";
+if ($_SERVER['HTTP_HOST'] === 'localhost' || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false) {
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "shopsphere";
+
+} else {
+
+    $host = "sql212.infinityfree.com";
+    $username = "if0_42638834";
+    $password = "ACKLT48Ur2Iuq0";
+    $database = "if0_42638834_shopsphere";
+
+}
 
 $conn = new mysqli($host, $username, $password, $database);
 
